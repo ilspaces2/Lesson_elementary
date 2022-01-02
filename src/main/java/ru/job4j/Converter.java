70a0665267a891fb8b10706efb38f7dabd0df044
I_M <moskva.88@mail.ru>
2022-01-02 19:59:53 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -35,21 +35,40 @@ public class Converter {
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + dollar + " dollar.");
-
         float rubleEuro = Converter.euroToRuble(2);
         System.out.println("2 euro are " + rubleEuro + " rubles.");
-
         float rubleDollar = Converter.dollarToRuble(2);
         System.out.println("2 dollar are " + rubleDollar + " rubles.");
-
         float dollarEuro = Converter.dollarToEuro(2);
         System.out.println("2 dollar are " + dollarEuro + " euro.");
-
         float euroDollar = Converter.euroToDollar(2);
         System.out.println("2 euro are " + euroDollar + " dollar.");
 
+        float in = 140;
+        float expected = 2;
+        boolean passed = expected == Converter.rubleToEuro(in);
+        System.out.println("140 rubles are 2 euro. Test result : " + passed);
+        float in1 = 140;
+        float expected1 = 2.3333333f;
+        boolean passed1 = expected1 == Converter.rubleToDollar(in1);
+        System.out.println("140 rubles are 2.3333333 dollar. Test result : " + passed1);
+        float in2 = 2;
+        float expected2 = 140;
+        boolean passed2 = expected2 == Converter.euroToRuble(in2);
+        System.out.println("2 euro are 140 rubles. Test result : " + passed2);
+        float in3 = 2;
+        float expected3 = 120;
+        boolean passed3 = expected3 == Converter.dollarToRuble(in3);
+        System.out.println("2 dollar are 120 rubles. Test result : " + passed3);
+        float in4 = 2;
+        float expected4 = 1.76f;
+        boolean passed4 = expected4 == Converter.dollarToEuro(in4);
+        System.out.println("2 dollar are 1.76 euro. Test result : " + passed4);
+        float in5 = 2;
+        float expected5 = 2.26f;
+        boolean passed5 = expected5 == Converter.euroToDollar(in5);
+        System.out.println("2 euro are 2.26 dollar. Test result : " + passed5);
     }
 }
