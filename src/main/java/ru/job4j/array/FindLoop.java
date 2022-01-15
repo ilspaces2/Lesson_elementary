e2d9a229a549cbf7eaae2eef3b5244eba1a3693a
I_M <moskva.88@mail.ru>
2022-01-15 15:15:15 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,15 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rsl = -1;
+        for (int i = start; i <= finish; i++) {
+            if (data[i] == el) {
+                rsl = i;
+                break;
+            }
+        }
+        return rsl;
+    }
 }
