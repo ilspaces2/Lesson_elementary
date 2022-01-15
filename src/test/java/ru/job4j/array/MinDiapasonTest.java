93ca12aa1fa68ac1d3d24ec5a57f2a81574d9286
I_M <moskva.88@mail.ru>
2022-01-15 19:51:25 +0300

    6.5.2. Поиск минимума в диапазоне. [#179804 #251199]

--- src/test/java/ru/job4j/array/MinDiapasonTest.java
+++ src/test/java/ru/job4j/array/MinDiapasonTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class MinDiapasonTest {
     @Test
     public void whenFirstMin() {
-        int[] array = new int[] {-1, 0, 5, 10};
+        int[] array = new int[]{-1, 0, 5, 10};
         int start = 1;
         int finish = 3;
         int result = MinDiapason.findMin(array, start, finish);
@@ -16,7 +16,7 @@ public class MinDiapasonTest {
 
     @Test
     public void whenLastMin() {
-        int[] array = new int[] {10, 5, 3, 1};
+        int[] array = new int[]{10, 5, 3, 1};
         int start = 1;
         int finish = 3;
         int result = MinDiapason.findMin(array, start, finish);
@@ -26,7 +26,7 @@ public class MinDiapasonTest {
 
     @Test
     public void whenMiddleMin() {
-        int[] array = new int[] {10, 2, 5, 1};
+        int[] array = new int[]{10, 2, 5, 1};
         int start = 0;
         int finish = 2;
         int result = MinDiapason.findMin(array, start, finish);
