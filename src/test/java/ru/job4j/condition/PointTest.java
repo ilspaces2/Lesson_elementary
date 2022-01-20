701f92c3285acb6f0804966adbde7e93dfa3a0ff
I_M <moskva.88@mail.ru>
2022-01-20 21:50:05 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,44 +8,36 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when41to27then6dot32() {
         double expected = 6.32;
-        int x1 = 4;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 7;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(4, 1);
+        Point b = new Point(2, 7);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when12to89then9dot89() {
         double expected = 9.89;
-        int x1 = 1;
-        int y1 = 2;
-        int x2 = 8;
-        int y2 = 9;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(1, 2);
+        Point b = new Point(8, 9);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 
     @Test
     public void when38to17then2dot23() {
         double expected = 2.23;
-        int x1 = 3;
-        int y1 = 8;
-        int x2 = 1;
-        int y2 = 7;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
+        Point a = new Point(3, 8);
+        Point b = new Point(1, 7);
+        double dist = a.distance(b);
+        Assert.assertEquals(expected, dist, 0.01);
     }
 }
